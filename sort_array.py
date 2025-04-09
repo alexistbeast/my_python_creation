a3d110db376e337356dce52496ea88e3bac344ec
alexistbeast <alexisgamer1515@gmail.com>
2025-04-09 16:08:32 +0200

    ajout de sort array

---
+++ sort_array.py
@@ -0,0 +1,23 @@
+def sort_array(source_array):
+    sorted_array = []
+    for i in source_array:
+        if i % 2 != 0:
+            sorted_array.append(i)
+    sorted_array.sort()
+    for index, valeur in enumerate(source_array):
+        if valeur % 2 == 0:
+            sorted_array.insert(index,valeur)
+    return sorted_array
+
+
+
+
+print(sort_array([5, 3, 2, 8, 1, 4]), [1, 3, 2, 8, 5, 4])
+print(sort_array([5, 3, 1, 8, 0]), [1, 3, 5, 8, 0])
+print(sort_array([]),[])
+print(sort_array([5, 3, 2, 8, 1, 4, 11]), [1, 3, 2, 8, 5, 4, 11])
+print(sort_array([2, 22, 37, 11, 4, 1, 5, 0]), [2, 22, 1, 5, 4, 11, 37, 0])
+print(sort_array([1, 111, 11, 11, 2, 1, 5, 0]),[1, 1, 5, 11, 2, 11, 111, 0])
+print(sort_array([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]),[1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
+print(sort_array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]),[0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+print(sort_array([0, 1, 2, 3, 4, 9, 8, 7, 6, 5]),[0, 1, 2, 3, 4, 5, 8, 7, 6, 9])
