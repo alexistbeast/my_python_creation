7b65e60eeb1f2289303c9b307fb185b0b33bc549
alexistbeast <alexisgamer1515@gmail.com>
2025-04-13 13:25:54 +0200

    ajout

--- range_extraction.py
+++ range_extraction.py
@@ -8,11 +8,9 @@ def solution(args):
         if i - (args.index(start)) >= 2: #donc si c'est une suite
             result.append(f"{start}-{args[i]}")
         else:
+            print(args.index(start),i)
             # Sinon on ajoute les éléments individuels
             for j in range(args.index(start), i + 1):
                 result.append(str(args[j]))
         i += 1
     return ",".join(result)
-
-
-print(solution([-6,-3,-2,-1,0,1,3,4,5,7,8,9,10,11,14,15,17,18,19,20]), '-6,-3-1,3-5,7-11,14,15,17-20')
